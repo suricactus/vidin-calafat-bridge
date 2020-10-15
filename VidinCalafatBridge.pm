a77006631440d279cb68b6d26f531c4aa27a1f49
Ivan Ivanov <ivan.ivanov@suricactus.com>
2020-10-15 14:28:53 +0300

    Cloudflare does not like it without UA

--- VidinCalafatBridge.pm
+++ VidinCalafatBridge.pm
@@ -40,7 +40,9 @@ sub Handler($)
 sub ExtractTraffic($)
 {
     my ($self) = @_;
-    my $ua = LWP::UserAgent->new;
+    my $ua = LWP::UserAgent->new( 
+        agent => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.123 Safari/537.36',
+    );
 
     $ua->timeout( $REQ_TIMEOUT );
 
